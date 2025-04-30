427cb37760bab285186f9c82aca48537f689c10b
sajidmanuu <123387252+sajidmanuu@users.noreply.github.com>
2025-04-30 17:48:07 +0530

    Delete backend directory

--- backend/routes/ballRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import ballController from '../controllers/ballController.js';
-
-const router = express.Router();
-
-// Ball Routes
-router.post('/ball', ballController.addBallDetails);
-
-export default router;
